9fcd575c6b9ba1013956c97f9e6f22f42b659020
Eduardo Stocchero de Oliveira <91025690+edurevsky@users.noreply.github.com>
2021-10-20 17:14:16 -0300

    Adicionado método validar dentro do init

--- desafio_string.py
+++ desafio_string.py
@@ -1,5 +1,6 @@
 import re
 
+
 class NotFoundError(Exception):
     pass
 
@@ -7,15 +8,15 @@ class NotFoundError(Exception):
 class BuscaCpf(object):
     def __init__(self, url: str):
         self.url = url
+        self.validar()
 
 
-    def verificar(self):
+    def validar(self):
         url = self.url
         igual = url.find("=")
-        base = url[0:igual]
         cpf = url[igual + 1:]
 
-        padrao = re.compile("[0-9]{3}(.)?[0-9]{3}(.)?[0-9]{3}(-)?[0-9]{2}")
+        padrao = re.compile("[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}")
         vePadrao = padrao.match(cpf)
 
         if not vePadrao:
@@ -23,5 +24,5 @@ class BuscaCpf(object):
         print(f"O CPF encontrado no link foi: {cpf}")
 
 
-BuscaCpf("https://www.bytebank.com.br/valida?cpf=12345678910").verificar()
-BuscaCpf("https://www.bytebank.com.br/valida?cpf=123.456.789-10").verificar()
+BuscaCpf("https://www.bytebank.com.br/valida?cpf=12345678910")
+BuscaCpf("https://www.bytebank.com.br/valida?cpf=123.456.789-10")
