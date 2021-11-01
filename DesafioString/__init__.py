49e98172b0640c207bd75e93f395152375517d5a
Eduardo Stocchero de Oliveira <91025690+edurevsky@users.noreply.github.com>
2021-11-01 11:13:24 -0300

    Update __init__.py

--- DesafioString/__init__.py
+++ DesafioString/__init__.py
@@ -1,12 +1,8 @@
 import re
 from validate_docbr import CPF
 
-
-class NotFoundError(Exception):
-    pass
-
-
 class VerificaCPF(object):
+    
     def __init__(self, url: str):
         self.url = url
         self.validar()
@@ -20,18 +16,16 @@ class VerificaCPF(object):
         padrao = re.compile("[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}")
         vePadrao = padrao.match(cpf)
 
-        if not vePadrao:
-            raise NotFoundError("Não foi padrão tipo CPF.")
-        print(f"O padrão CPF encontrado no link foi: {cpf}")
-
         try:
-            validacao = CPF()
-            if validacao.validate(cpf):
-                print("O CPF é real.\n")
+            if not vePadrao:
+                print("Não foi encontrado um padrão tipo CPF.")
             else:
-                print("O CPF é falso.\n")
-            
+                print(f"O padrão CPF encontrado no link foi: {cpf}")
+                validacao = CPF()
+                if validacao.validate(cpf):
+                    print("O CPF é real.\n")
+                else:
+                    print("O CPF é falso.\n")
+                
         except:
             pass
-
-
